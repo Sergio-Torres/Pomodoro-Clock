9f80b94954a5df7ea2bf323f0740c4be16058397
Sergio Torres <sergiotorresca77@gmail.com>
2021-11-09 00:57:35 -0500

    Functionality has been added that allows to automatically start the countdown to rest when the session time is up, but does not automatically start the new session when the rest ends

--- index.js
+++ index.js
@@ -7,24 +7,45 @@ const sessionLength = document.getElementById("session-length");
 const sessionIncrement = document.getElementById("session-increment");
 // timer
 const time = document.getElementById("time");
+const timeLabel = document.getElementById("timer-label");
 //controllers 
 const startStop = document.getElementById("start-stop");
 const buttonReset = document.getElementById("button-reset");
 
-let breakTime =5;
-let sessionTime = 25;
-let count;
-//Settings decrement and increment (break-length and session-length)
+let breakTime=5;
+let sessionTime=25;
+let count = sessionTime * 60;
+let countBreak= breakTime * 60;
+
+//*-Settings decrement and increment (break-length and session-length)-*//
 breakDecrement.onclick = ()=>{
-    breakTime--;
+    if(breakTime>3){
+        breakTime--;
+    }
+    else{
+        breakTime = 3;
+    }
+    countBreak = breakTime *60;
     console.log('breakTime', breakTime);
     breakLength.innerHTML = breakTime;
+   
+    
 };
+
 breakIncrement.onclick = ()=>{
-    breakTime++;
+
+    if(breakTime<20){
+        breakTime++;
+    }
+    else{
+        breakTime = 20;
+    }
+    countBreak = breakTime *60;
     console.log('breakTime', breakTime);
     breakLength.innerHTML = breakTime;
+    
 };
+
 sessionDecrement.onclick = ()=>{
     //minimum working time
     if(sessionTime>10){
@@ -33,11 +54,10 @@ sessionDecrement.onclick = ()=>{
     else{
         sessionTime = 10;
     }
-    count  = sessionTime * 60; 
+    count  = sessionTime * 60;         
     console.log('sessionTime', sessionTime);
     sessionLength.innerHTML = sessionTime;
     time.innerHTML = sessionTime + ":00";
-    
 }
 sessionIncrement.onclick = ()=>{
     //maximum working time
@@ -47,7 +67,7 @@ sessionIncrement.onclick = ()=>{
     else{
         sessionTime = 60;
     }
-    
+        
     count  = sessionTime * 60; 
     console.log('sessionTime', sessionTime);
     sessionLength.innerHTML = sessionTime;
@@ -55,26 +75,61 @@ sessionIncrement.onclick = ()=>{
 }
 
 
-let interval = -1;
+//*-----Timer functionalities---------------*//
 
-function loadClick(){
-    startStop.onclick = countDown;   
+
+let intervalBreak;
+let interval = -1;
+  
+startStop.onclick = countDown;   
+
+function breakWork(){
+    
+    clearInterval(interval);
+    timeLabel.innerHTML = "Break"; 
+
+    intervalBreak =setInterval(function(){    
+
+        const minutesBreak = Math.floor(countBreak/60);
+        let secondsBreak = countBreak % 60;
+        secondsBreak = secondsBreak <10 ? '0'+secondsBreak: secondsBreak;
+        console.log('minutes & seconds', minutesBreak, ':', secondsBreak);
+        time.innerHTML = `${minutesBreak}:${secondsBreak}`;
+        //Whe the time is up, it will call de countDown function
+        if(minutesBreak==0 && secondsBreak ==1){
+            count  = sessionTime * 60;       
+            let callSession = countDown();
+            return callSession;
+        }
+        countBreak--;
+        console.log('intervalBreak',intervalBreak);
+    },1000); 
 }
 
-function countDown(){     
-    if(interval ==-1){
-        
+function countDown(){  
+   
+    clearInterval(intervalBreak);
+    timeLabel.innerHTML = "Session"; 
+    if(interval ==-1){     
+           
         interval =setInterval(function(){
-                
+                 
                 const minutes = Math.floor(count/60);
-                let seconds = count % 60;
+                let seconds= count % 60;
                 seconds = seconds <10 ? '0'+seconds: seconds;
-                startStop.innerHTML = "Stop";
+                startStop.innerHTML = "Pause";
                 time.innerHTML = `${minutes}:${seconds}`;
+                //when the countDown finishes, it will call the rest function
+                if(minutes==0 && seconds ==1){
+                    let callBreak = breakWork();
+                    return callBreak;
+                }
+            
                 count--;
                 console.log('interval',interval);
-            },1000);   
-    } 
+                
+            },1000);             
+    }   
     else{
         startStop.innerHTML = "Start";
         clearInterval(interval);
@@ -82,12 +137,11 @@ function countDown(){
     } 
       
 };
-    
+   
 buttonReset.onclick = ()=>{
     count  = sessionTime * 60;
     console.log('count',count);
     time.innerHTML = sessionTime + ":00"
     countDown;
-
 }
-loadClick();
+
